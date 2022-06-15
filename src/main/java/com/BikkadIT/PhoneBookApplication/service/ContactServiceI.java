8b01a2c8edba30f49af3b0eb240225d2cd07d262
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-06-15 09:53:47 -0800

    PHON-09:

--- src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceI.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/service/ContactServiceI.java
@@ -1,5 +1,8 @@
 package com.BikkadIT.PhoneBookApplication.service;
 
+import com.BikkadIT.PhoneBookApplication.model.Contact;
+
 public interface ContactServiceI {
 
+	boolean saveContact(Contact contact);
 }
